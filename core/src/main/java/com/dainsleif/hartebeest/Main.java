262329978b75e2bc3a364490f9884ad892512fb1
jonvicbarcenas <exchantpro@gmail.com>
2025-03-10 22:46:57 +0800

    Add ScreenExample class and player data; update game initialization

--- core/src/main/java/com/dainsleif/hartebeest/Main.java
+++ core/src/main/java/com/dainsleif/hartebeest/Main.java
@@ -1,13 +1,14 @@
 package com.dainsleif.hartebeest;
 
 import com.badlogic.gdx.Game;
+import com.dainsleif.hartebeest.screens.ScreenExample;
 import com.dainsleif.hartebeest.world.Gameworld1;
 
 public class Main extends Game {
 
     @Override
     public void create() {
-        this.setScreen(new Gameworld1());
+        this.setScreen(new ScreenExample());
     }
 
 
