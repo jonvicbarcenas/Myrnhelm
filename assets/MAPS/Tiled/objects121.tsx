6c20c554be8076ab0a1fabe715ba13dfb562059b
Tharzzzs <aliamyrondeandre@gmail.com>
2025-03-15 20:42:52 +0800

    march 15, 2025

---
+++ assets/MAPS/Tiled/objects121.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="objects121" tilewidth="64" tileheight="48" tilecount="16" columns="4">
+ <image source="../Mystic Woods/sprites/objects/objects.png" trans="ffffff" width="256" height="208"/>
+</tileset>
