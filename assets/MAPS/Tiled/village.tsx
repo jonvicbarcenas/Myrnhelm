899ab7d9d83644583f5a581238dc99fbe3b93dae
Tharzzzs <aliamyrondeandre@gmail.com>
2025-03-18 20:53:07 +0800

    march 18, 2025

---
+++ assets/MAPS/Tiled/village.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="villagepreview" tilewidth="16" tileheight="16" tilecount="729" columns="27">
+ <image source="../../../../MAPS/Tiled/valerio-colonna-tilesets-topdown-removebg-preview.png" trans="ffffff" width="432" height="432"/>
+</tileset>
